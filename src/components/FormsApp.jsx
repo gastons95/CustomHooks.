f437c0a3ca221137a289f1249e32ca2dc78b9b41
Gaston Suarez <gastonsbu@gmail.com>
2025-04-05 23:37:30 -0300

    Hicimos este repositorio para guardar 3 hooks personalizados sobre contadores, formularios y Fetch

---
+++ src/components/FormsApp.jsx
@@ -0,0 +1,61 @@
+import { useForm } from "../hooks/useForm"
+
+export const FormsApp = () => {
+
+    const initialForm = {
+        username: '',
+        email: '',
+        password: ''
+    }
+
+
+    const { username, email, password, onInputChange } = useForm(initialForm)
+
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        console.log(username,email,password)
+    }
+
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <div className="mb-3">
+                    <label htmlFor="username" className="form-label">UserName</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="username"
+                        name='username'
+                        value={username}
+                        onChange={onInputChange}
+                    />
+                </div>
+
+                <div className="mb-3">
+                    <label htmlFor="email" className="form-label">Email</label>
+                    <input
+                        type="email"
+                        className="form-control"
+                        id="email"
+                        name='email'
+                        value={email}
+                        onChange={onInputChange}
+                    />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="password" className="form-label">Password</label>
+                    <input
+                        type="password"
+                        className="form-control"
+                        id="pasword"
+                        name='pasword'
+                        value={password}
+                        onChange={onInputChange}
+                    />
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+        </>
+    )
+}
